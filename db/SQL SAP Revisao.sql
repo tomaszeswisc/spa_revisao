730c81dbe10c25090fb19afd37e962531fb56313
tomaszeswisc <hiltonelias@senai-pi.com.br>
2026-06-10 10:45:59 -0300

    Inicio DB MYSQL

---
+++ db/SQL SAP Revisao.sql
@@ -0,0 +1,29 @@
+/* Cria o banco de dados */
+CREATE DATABASE atividade_single_page;
+
+/* Seleciona o banco que será utilizado */
+USE atividade_single_page;
+
+/* --------------Criar Tabela de Produtos-------------- */
+
+CREATE TABLE produtos(
+
+/* identificador único*/
+id INT auto_increment primary key,
+
+/*Nome do Produto*/
+nome varchar(100),
+
+/* categoria */
+categoria varchar(100),
+
+/*descricao*/
+descricao TEXT
+
+);
+
+
+/* Mostrar os campos da tabela */
+
+describe produtos;
+
