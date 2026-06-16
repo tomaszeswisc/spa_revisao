6a7d5d7c64f3ea3f98b7fb4e1b0ee01e6553ec14
tomaszeswisc <hiltonelias@senai-pi.com.br>
2026-06-15 21:09:15 -0300

    Rotas Iniciais

--- server.js
+++ server.js
@@ -57,3 +57,92 @@ app.listen(3000,function(){
     /*Exibir menssagem de servidor ativo */
     console.log("Servidor esta rodando em http://localhost:3000");
 });
+
+
+/*------------------------ Rotas para teste --------------------------------*/
+/* Rota GET para Listar os Produtos */
+
+app.get("/produtos", function(req, res){
+    /*Comando SQL de busca de produtos */
+    const sql ="select * from produtos";
+
+    /*Executar a busca */
+    db.query(sql, function(erro, resultados){
+
+        /*Verificar se ocorreu erro */
+
+        if(erro){
+            /*Retorna o erro para o Cliente */
+            return res.status(500).json({
+                erro:"Erro ao buscar produtos"
+            });
+        }
+
+        /*Retorna os produtos encontrados */
+        res.status(200).json(resultados);
+    });
+
+});
+
+// --------------- Rota Para Buscar produto por ID -----------------
+//Rota GET para buscar um produto específico
+
+app.get("/produtos/:id", function(req, res){
+    //Capturar o id envidado pela URL
+    const id = req.params.id;
+    //Comando SQL filtrando pelo ID
+    const sql = "select * from produtos where id = ?";
+    //Executat a consulta
+    db.query(sql, [id], function(erro, resultados){
+        //verifica erro
+        if(erro){
+            //Retorna erro para o cliente
+            return res.status(500).json({
+                erro:"Erro ao buscar o produto"
+            });
+        }
+        //verifica se encontrou algum registro
+        if(resultados.length === 0){
+            //Retorna mensagem de não encontrado
+            return res.status(404).json({
+                menssagem:"Produtdo não encontrado!"
+            });
+        }
+        //Retorna o produto encontrado
+        res.status(200).json(resultados[0]);
+    });
+});
+
+
+//------------ Cadastrar Produto ----------------
+
+app.post("/produtos", function(req, res){
+    //cria todas constantes de uma so vez
+ 
+    //const {nome, categoria, descrição} = req.body
+
+    //Capturar o nome enviado
+    const nome = req.body.nome;
+    //Capturar a categoria
+    const categoria = req.body.categoria;
+    //Capturar a descrição
+    const descricao = req.body.descricao;
+
+    //Criar o SQL de inserção
+    const sql = `insert into produtos (nome, categoria, descricao) values(?,?,?)`;
+    //Executar o cadastro
+    db.query(sql, [nome, categoria, descricao], function(erro, resultado){
+        //verifica o erro
+        if(erro){
+            //Retorna erro para o cliente
+            return res.status(500).json({
+                erro: "Erro ao cadastrar o produto"
+            });
+        }
+    //Retorna mensagem de sucesso
+        res.status(201).json({
+            menssagem:"Produto cadastrado com sucesso!",
+            id:resultado.insertId
+        });
+    });
+});
